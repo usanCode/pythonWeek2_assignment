8df784c31add2eec51d71b47e2451f5dd0995c19
usanCode <sabinerugugula@gmail.com>
2025-03-24 00:02:11 +0200

    responding to questions

---
+++ answers.py
@@ -0,0 +1,17 @@
+
+# 1. Create an empty list called my_list.
+my_list = []
+# 2. Append the following elements to my_list: 10, 20, 30, 40.
+my_list.extend([10,20,30,40])
+# 3. Insert the value 15 at the second position in the list.
+my_list.insert(1, 15)
+# 4. Extend my_list with another list: [50, 60, 70].
+my_list.extend([50,60,70])
+# 5. Remove the last element from my_list.
+my_list.pop()
+# Sort my_list in ascending order.
+my_list.sort()
+# Find and print the index of the value 30 in my_list.
+print(my_list.index(30))
+ 
+
